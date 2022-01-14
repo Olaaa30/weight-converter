9a358ea33df47b3fee7dc9f7eae18f75aea81eb6
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-01-14 14:29:54 +0100

    built the logic from scratch

---
+++ app.py
@@ -0,0 +1,18 @@
+run = True
+user_weight = float(input('weight:'))
+while run:
+    selection = input('pounds(l) or kilos(k)? ')
+
+    if selection.lower() == 'k': 
+        weight = user_weight * 2.20462
+        print(f'your weight in pounds is {weight}')
+        run = False
+
+    elif selection.lower() == 'l':
+        weight = user_weight * 0.4535
+        print(f'your weight in kilos is {weight}')
+        run = False
+
+    else:
+        print('wrong input!!! Please input l or k')
+
